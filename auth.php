50a601d30eacee0e15f9e013d058f905ea1d97f7
introqt <introtheintro@gmail.com>
2017-02-03 15:51:30 +0200

    var dump

--- auth.php
+++ auth.php
@@ -1,8 +1,5 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var_dump(input_type_file($_POST['login']));
+var_dump(input_type_file($_POST['password']));
 
